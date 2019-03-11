c3715e2b7a7b3e97402503a9823bcd354dc8e459
ryanphillipthomas <ryanphillipthomas@mac.com>
2019-03-11 19:54:02 -0400

    Setup

---
+++ What Would Ozzy Say/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  What Would Ozzy Say
+//
+//  Created by Ryan Thomas on 5/4/18.
+//  Copyright © 2018 Ryan Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ What Would Ozzy SayTests/What_Would_Ozzy_SayTests.swift
@@ -0,0 +1,36 @@
+//
+//  What_Would_Ozzy_SayTests.swift
+//  What Would Ozzy SayTests
+//
+//  Created by Ryan Thomas on 5/4/18.
+//  Copyright © 2018 Ryan Thomas. All rights reserved.
+//
+
+import XCTest
+@testable import What_Would_Ozzy_Say
+
+class What_Would_Ozzy_SayTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
